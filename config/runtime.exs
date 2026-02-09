ad43927e4450e8572f030dfed534806bd7f6e399
Rinku Kumari <rinkuk.official@gmail.com>
2026-02-09 18:16:13 +0530

    Deploy fly new (#1)
    * db change
    
    * Update mix.exs
    
    * Update runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -85,7 +85,7 @@ if config_env() == :prod do
     [
       url: database_url,
       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-      socket_options: maybe_ipv6
+      
     ]
   end
 
