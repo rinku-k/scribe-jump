b342347f60f2a1d027bcbb166b59c4afc668bb03
Rinku Kumari <rinkuk1993@gmail.com>
2026-02-09 18:18:58 +0530

    Update mix.exs with explicit releases config and revert Procfile to clean command

--- mix.exs
+++ mix.exs
@@ -9,7 +9,13 @@ defmodule SocialScribe.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      releases: [
+        social_scribe: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent]
+        ]
+      ]
     ]
   end
 
